582c8da565a021e08a26c977619fa0d3c20e15f3
Noah Fisher <noahfisherxd@gmail.com>
2023-09-14 14:44:41 -0700

    Deploy Website

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ library.add(fab, fas);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter basename="/">
       <Routes>
         <Route path="/*" element={<App />} />
       </Routes>
