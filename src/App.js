69a72f939c6d992a4ce1d1e1646cbeebd6473895
Noah Fisher <noahfisherxd@gmail.com>
2023-09-14 12:16:21 -0700

    Deploying Website

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
-import Layout from "./components/Layout";
 import Main from "./components/Main";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Layout />}>
         <Route index element={<Main />} />
-      </Route>
     </Routes>
   );
 }
--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ library.add(fab, fas);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/*" element={<App />} />
       </Routes>
